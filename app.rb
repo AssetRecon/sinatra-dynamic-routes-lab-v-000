9264aad0fb9840b223c0d0065d8af03c59aba826
Edsel Caprice <caprice@mail.usf.edu>
2018-10-15 11:01:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,19 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+get '/reversename/:name' do
+  @user_name = params[:name]
+  reversename = @user_name.reverse
+  reversename
+end
+
+get '/square/:number' do
+  @number = params[:number]
+  square = @number.to_i*@number.to_1
+  square
+end
+
+
+
+
 end
