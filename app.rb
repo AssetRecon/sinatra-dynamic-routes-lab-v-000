b1b04f5b830da7fd9ad56abdd4db4b2b16508241
Edsel Caprice <caprice@mail.usf.edu>
2018-10-15 11:04:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,15 @@ get '/square/:number' do
   square
 end
 
+get '/say/:number/:phrase' do
+  @number = params[:number].to_i
+  @phrase = params[:phrase]
 
+  @numbers.times do
+    @phrase
+  end
+  
+end
 
 
 end
