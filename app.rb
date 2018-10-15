0b7aedc957ae52f6d58b50c8736660b662104a6a
Edsel Caprice <caprice@mail.usf.edu>
2018-10-15 11:25:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,9 +30,21 @@ end
   end
 
 get '/:operation/:number1/:number2' do
-  @operation = params[:operation]
-  @number1 = params[:operation].to_i
-  @number2 = params[:operation].to_i
+  operation = params[:operation]
+  number1 = params[:operation].to_i
+  number2 = params[:operation].to_i
+
+  if operation = "add"
+    answer = number1 + number2
+  elsif operation = "subtract"
+    answer = number1 - number2
+  elsif operation = "multiply"
+    answer = number1 * number2
+  elsif operation = "divide"
+    answer = number1 / number2
+  end
+  anser
+
 end
 
 
